869c735a24910934f09abd55fe2e13f0ed68be76
蔡巧柔 <100175502+c109111124@users.noreply.github.com>
2022-11-16 13:35:44 +0800

    Add files via upload

---
+++ 111-1QZ3/Test.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1QZ3
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        string[] s_City = new string[3] { "台北市", "新北市", "台中市" };
+        string[,] s_Area = new string[3, 3] {
+            {"中正區", "文山區", "大安區"},
+            {"淡水區", "石碇區", "土城區"},
+            {"西屯區", "北屯區", "東區"}
+        };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                for (int i_Ct = 0; i_Ct < s_City.Length; i_Ct++)
+                {
+                    ListItem o_L = new ListItem();
+                    o_L.Text = o_L.Value = s_City[i_Ct];
+
+                    dpl_City.Items.Add(o_L);
+                }
+            }
+            mt_GenSecondList();
+        }
+        protected void dpl_City_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mt_GenSecondList();
+        }
+
+        protected void mt_GenSecondList()
+        {
+            int i_ind = dpl_City.SelectedIndex;
+            dpl_Area.Items.Clear();
+            for (int i_Ct = 0; i_Ct < s_Area.GetLength(1); i_Ct++)//sa_2D.GetLength(1)是抓取sa_2D[(0),(1)]
+            {
+                ListItem o_L = new ListItem();
+                o_L.Text = o_L.Value = s_Area[i_ind, i_Ct];
+                dpl_Area.Items.Add(o_L);
+            }
+        }
+
+        protected void rbl_Phone_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (rbl_Phone.SelectedIndex == 2)
+            {
+                txt_Phone.Visible = false;
+            }
+            else
+            {
+                txt_Phone.Visible = true;
+            }
+        }
+    }
+}
---
+++ 111-1QZ3/Test_Sub.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1QZ3
+{
+    public partial class Test_Sub : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] s_Get = new string[] { "tb_Num", "rbl_Phone", "txt_Phone", "dpl_City", "dpl_Area" };
+            string[] s_Menu = new string[] { "保單號碼: ", "通訊種類: ", "通訊號碼: ", "所在城市: ", "所在區域: " };
+            for (int i_Ct = 0; i_Ct < s_Get.Length; i_Ct++)
+            {
+                if (Request.Form.Get(s_Get[i_Ct]) == null)
+                {
+                    continue;
+                }
+                lb_Msg.Text += s_Menu[i_Ct] + Request.Form.Get(s_Get[i_Ct]) + "<br />";
+                Response.Write(Request.Form.Get(s_Get[i_Ct]));
+            }
+        }
+    }
+}
